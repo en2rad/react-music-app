00dc0eb6ec2df5df55a6260f300800e75bd9c163
Aleksey Ramanovich <en2rad@mail.ru>
2021-04-19 17:07:31 +0300

    feat: swipper img

--- src/components/Player/SwiperNew.jsx
+++ src/components/Player/SwiperNew.jsx
@@ -1,126 +1,60 @@
-import React, { useState } from 'react';
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination, Scrollbar, EffectCoverflow} from 'swiper';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination, Controller, Thumbs } from 'swiper';
+
+// Import Swiper styles
+// import 'swiper/swiper.scss';
+// import 'swiper/components/navigation/navigation.scss';
+// import 'swiper/components/pagination/pagination.scss';
+// import 'swiper/components/scrollbar/scrollbar.scss';
 import 'swiper/swiper-bundle.css';
-// import './styles.css';
-import { connect } from 'react-redux';
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, EffectCoverflow]);
 
-import { 
-  handleProgress, 
-} from "../../redux/action";
-
-SwiperCore.use([Navigation, Pagination, Controller, Thumbs]);
-
-function SwiperNew(playerState, playList) {
-
-  const {   
-    currentPlayList,
-    currentSongIndex,
-    playing,
-    muted,
-    volume,
-    played,
-    loop,
-} = playerState;
-
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
-//   const [controlledSwiper    , seatControlledSwiper] = useState(null);
-
-//   const slides = [];
-//   for (let i = 0; i < 5; i += 1) {
-//     slides.push(
-//         <SwiperSlide key={`slide-${i}`} tag="li">
-//             <img
-//             src={`https://picsum.photos/id/${i + 1}/500/300`}
-//             style={{ listStyle: 'none' }}
-//             alt={`Slide ${i}`}
-//             />
-//         </SwiperSlide>
-//     );
-//   }
-
-
- 
-  const thumbs = [];
-  for (let i = 0; i < 3; i += 1) {
-    thumbs.push(
-        <SwiperSlide key={`thumb-${i}`} tag="li" style={{ listStyle: 'none' }}>
-            <img
-            src={`./images/mujuice.jpg`}
-            alt={`Thumbnail ${i}`}
-            ></img>
-        </SwiperSlide>
-    );
-  }
-
-  const src = "./images/mujuice.jpg"
+function SwiperNew() {
+	const src = "./images/mujuice.jpg"
 
   return (
-    <React.Fragment>
-        <div className="lel">
-      <Swiper
-        id="thumbs"
-        spaceBetween={5}
-        slidesPerView={3}
-        onSwiper={setThumbsSwiper}
-      >
-        {thumbs}
-      </Swiper>
-
-    
-	<Swiper
-	className="c-player-slider"
-	// spaceBetween={10}
-	slidesPerView={1}	
-	navigation
-	pagination
-	// onClick={() => onSkipTrack(false)}
-	// onSlideChange={() => onSkipTrack()}
-	// onSwiper={(swiper) => onSkipTrack(swiper)}
-	>
-	\
-
-		{/* {currentPlayList.currentSongIndex ?
-			<>
-			<SwiperSlide slot="container-start" className="c-player-slider__slide" >
-			<img className="c-player-info__img" src={currentPlayList.currentSongIndex.img_src} title="img"/>
+	<div className="wraps">
+    <Swiper
+	initialSlide={1}
+    //   spaceBetween={50}
+    centerSlides
+ 
+	  effect="coverflow"
+      pagination={{ clickable: true }}
+    //   scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+		
+			<SwiperSlide >	
+				<img
+					src={`https://picsum.photos/id/${1}/163/100`}
+					alt={`Thumbnail ${1}`}
+				>
+				</img>
 			</SwiperSlide>
-			
-			<SwiperSlide slot="wrapper-end" className="c-player-slider__slide" >
-				<img className="c-player-info__img" src={currentPlayList.currentSongIndex.img_src} title="img"/>
+			<SwiperSlide initialSlide={1}>	
+				<img
+					src={`https://picsum.photos/id/${2}/163/100`}
+					alt={`Thumbnail ${2}`}
+				>
+				</img>
 			</SwiperSlide>
-
-
-			<SwiperSlide slot="wrapper-end" className="c-player-slider__slide" >
-				<img className="c-player-info__img" src={currentPlayList.currentSongIndex.img_src} title="img"/>
+			<SwiperSlide>	
+				<img
+					src={`https://picsum.photos/id/${3}/163/100`}
+					alt={`Thumbnail ${3}`}
+				>
+				</img>
 			</SwiperSlide>
-			</>
-			:
-			null
-		}
-	 */}
-	</Swiper>
-
-
-        </div>
-    </React.Fragment>
+		
+    </Swiper>
+	</div>  
   );
-}
-
-
-
-const mapStateToProps = (store) => {
-  const { playList } = store;
-  const { playerState } = store;
-  return {
-      playerState: playerState,
-      playList: playList,
-  };
 };
 
-export default connect(
-  mapStateToProps, 
-  { 
-      handleProgress, 
-  }
-)(SwiperNew);
+
+export default SwiperNew 
